35ace258762aa42b1290a5f67c694e2d308a90cb
田瑜 <515301230@qq.comm>
2018-09-14 17:45:30 +0800

    ......

---
+++ src/view/index/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    import {  } from 'vant';
+
+    export default {
+        components: {
+
+        },
+
+        data() {
+            return {
+
+            };
+        },
+
+        computed: {
+
+        },
+
+        methods: {
+
+        }
+    };
+</script>
+
+<style lang="less">
+
+</style>
